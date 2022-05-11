7ab70214aeecaa32c6e57922fcc5f01cae51a30c
Khagendrabattrai <bhattaraikhagendra56@gmail.com>
2022-05-10 22:51:13 -0500

    Day2 To Day 5 Class Demo files added

---
+++ Src/day4class/Subtraction.java
@@ -0,0 +1,13 @@
+package day4class;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+int number1= 7;
+int number2=3;
+int sub= number1 - number2;
+System.out.println("Subtrsction of number1 and number2 is:"+sub);
+
+	}
+
+}
---
+++ Src/day5basic/Addition.java
@@ -0,0 +1,18 @@
+package day5basic;
+
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+Scanner input=new Scanner(System.in);
+System.out.println("Enter number1:");
+int number1=input.nextInt();
+System.out.println("Enter number2:");
+int number2=input.nextInt();
+int sum=number1 + number2;
+System.out.println("Sum of number1 and number2 is:"+sum);
+input.close();
+}
+
+}
